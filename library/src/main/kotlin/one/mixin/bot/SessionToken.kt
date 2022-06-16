3f5f19f8961f0bbfe877f3f20257d99ed3e0b5ea
Crossle Song <crosslesong@gmail.com>
2022-06-16 08:49:19 +0400

    Fix typo

--- library/src/main/kotlin/one/mixin/bot/SessionToken.kt
+++ library/src/main/kotlin/one/mixin/bot/SessionToken.kt
@@ -2,7 +2,6 @@ package one.mixin.bot
 
 import net.i2p.crypto.eddsa.EdDSAPrivateKey
 import one.mixin.bot.extension.base64Encode
-import one.mixin.bot.vo.User
 import java.security.Key
 import java.security.KeyPair
 
@@ -20,12 +19,12 @@ sealed class SessionToken(open val userId: String, open val sessionId: String) {
     ) : SessionToken(userId, sessionId)
 }
 
-fun getUserSessionToken(user: User, sessionKey: KeyPair, isRsa: Boolean = false): SessionToken {
+fun getUserSessionToken(userId: String, sessionId: String, sessionKey: KeyPair, isRsa: Boolean = false): SessionToken {
     return if (isRsa) {
-        SessionToken.RSA(user.userId, user.sessionId, sessionKey.private)
+        SessionToken.RSA(userId, sessionId, sessionKey.private)
     } else {
         SessionToken.EdDSA(
-            user.userId, user.sessionId,
+            userId, sessionId,
             (sessionKey.private as EdDSAPrivateKey).seed.base64Encode()
         )
     }
