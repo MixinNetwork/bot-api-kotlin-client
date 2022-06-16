57051def20ad8c37d9ce7d3925e6fe76d4fb4b7c
Crossle Song <crosslesong@gmail.com>
2022-06-16 08:47:56 +0400

    More session token function

--- library/src/main/kotlin/one/mixin/bot/SessionToken.kt
+++ library/src/main/kotlin/one/mixin/bot/SessionToken.kt
@@ -1,6 +1,10 @@
 package one.mixin.bot
 
+import net.i2p.crypto.eddsa.EdDSAPrivateKey
+import one.mixin.bot.extension.base64Encode
+import one.mixin.bot.vo.User
 import java.security.Key
+import java.security.KeyPair
 
 sealed class SessionToken(open val userId: String, open val sessionId: String) {
     data class RSA(
@@ -14,4 +18,15 @@ sealed class SessionToken(open val userId: String, open val sessionId: String) {
         override val sessionId: String,
         val seed: String
     ) : SessionToken(userId, sessionId)
+}
+
+fun getUserSessionToken(user: User, sessionKey: KeyPair, isRsa: Boolean = false): SessionToken {
+    return if (isRsa) {
+        SessionToken.RSA(user.userId, user.sessionId, sessionKey.private)
+    } else {
+        SessionToken.EdDSA(
+            user.userId, user.sessionId,
+            (sessionKey.private as EdDSAPrivateKey).seed.base64Encode()
+        )
+    }
 }
