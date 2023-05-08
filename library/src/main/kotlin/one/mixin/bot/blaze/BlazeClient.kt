f80aa68d2940a6ae3c86b66c4cd5e2deca8de379
airhead <L4qiang@gmail.com>
2023-05-08 23:09:59 +0800

    fix(library): extra code to OkHttpProvider.kt

--- library/src/main/kotlin/one/mixin/bot/blaze/BlazeClient.kt
+++ library/src/main/kotlin/one/mixin/bot/blaze/BlazeClient.kt
@@ -2,23 +2,14 @@ package one.mixin.bot.blaze
 
 import com.google.gson.Gson
 import net.i2p.crypto.eddsa.EdDSAPrivateKey
-import net.i2p.crypto.eddsa.spec.EdDSANamedCurveTable
-import net.i2p.crypto.eddsa.spec.EdDSAPrivateKeySpec
 import okhttp3.* //ktlint-disable
-import okhttp3.logging.HttpLoggingInterceptor
 import okio.ByteString
 import one.mixin.bot.Constants
 import one.mixin.bot.SessionToken
 import one.mixin.bot.blaze.msg.Cards
-import one.mixin.bot.extension.base64Decode
 import one.mixin.bot.extension.base64Encode
-import one.mixin.bot.signToken
-import one.mixin.bot.util.base64Encode
-import one.mixin.bot.util.decodeAs
-import one.mixin.bot.util.encode
-import one.mixin.bot.util.getRSAPrivateKeyFromString
+import one.mixin.bot.util.* //ktlint-disable
 import java.util.*
-import java.util.concurrent.TimeUnit
 
 class BlazeClient private constructor(
     private val clientToken: SessionToken,
@@ -27,6 +18,7 @@ class BlazeClient private constructor(
     private val parseData: Boolean = false,
     private val autoAck: Boolean = false,
     debug: Boolean = false,
+    autoSwitch: Boolean = false,
 ) : WebSocketListener() {
     private var isConnected: Boolean = false
     private var connectNum = 0
@@ -35,23 +27,9 @@ class BlazeClient private constructor(
     private val DEFAULT_INTERVAL = 5000 // 重连间隔时间，毫秒
 
     private var userSessionToken: SessionToken? = null
-    private val ed25519 by lazy { EdDSANamedCurveTable.getByName(EdDSANamedCurveTable.ED_25519) }
 
     private val okHttpClient: OkHttpClient by lazy {
-        val builder = OkHttpClient.Builder()
-        if (debug) {
-            val logging = HttpLoggingInterceptor()
-            logging.level = HttpLoggingInterceptor.Level.BODY
-            builder.addNetworkInterceptor(logging)
-        }
-        builder.connectTimeout(10, TimeUnit.SECONDS)
-        builder.writeTimeout(10, TimeUnit.SECONDS)
-        builder.readTimeout(10, TimeUnit.SECONDS)
-        builder.pingInterval(15, TimeUnit.SECONDS)
-        builder.retryOnConnectionFailure(false)
-        builder.connectionPool(ConnectionPool(100, 1, TimeUnit.SECONDS))
-
-        builder.build()
+        createHttpClient(userSessionToken, clientToken, true, debug, cnServer, autoSwitch)
     }
 
     private var webSocket: WebSocket? = null
@@ -65,30 +43,13 @@ class BlazeClient private constructor(
             isConnected = false
         }
 
-        val request = Request.Builder().addHeader("Sec-WebSocket-Protocol", "MixinBot-Blaze-1")
-            .addHeader(
-                "Authorization",
-                "Bearer " + (userSessionToken ?: clientToken).let { token ->
-                    signToken(
-                        token.userId, token.sessionId, "GET", "/", "",
-                        if (token is SessionToken.RSA) {
-                            token.privateKey
-                        } else {
-                            val seed = (token as SessionToken.EdDSA).seed
-                            val privateSpec = EdDSAPrivateKeySpec(
-                                seed.base64Decode(), ed25519
-                            )
-                            EdDSAPrivateKey(privateSpec)
-                        }
-                    )
-                }
-            ).url(
-                if (cnServer) {
-                    Constants.API.CN_WS_URL
-                } else {
-                    Constants.API.WS_URL
-                }
-            ).build()
+        val request = Request.Builder().url(
+            if (cnServer) {
+                Constants.API.CN_WS_URL
+            } else {
+                Constants.API.WS_URL
+            }
+        ).build()
         webSocket = okHttpClient.newWebSocket(request, this)
     }
 
@@ -115,20 +76,12 @@ class BlazeClient private constructor(
         private var autoAck: Boolean = false
         private var blazeHandler: BlazeHandler = DefaultBlazeHandler()
 
-        fun configEdDSA(
-            userId: String,
-            sessionId: String,
-            privateKey: EdDSAPrivateKey
-        ): Builder {
+        fun configEdDSA(userId: String, sessionId: String, privateKey: EdDSAPrivateKey): Builder {
             clientToken = SessionToken.EdDSA(userId, sessionId, privateKey.seed.base64Encode())
             return this
         }
 
-        fun configRSA(
-            userId: String,
-            sessionId: String,
-            privateKey: String
-        ): Builder {
+        fun configRSA(userId: String, sessionId: String, privateKey: String): Builder {
             val key = getRSAPrivateKeyFromString(privateKey)
             clientToken = SessionToken.RSA(userId, sessionId, key)
             return this
@@ -185,7 +138,7 @@ class BlazeClient private constructor(
                     sendAckMsg(webSocket, blazeMsg.data?.messageId!!)
                 }
             }
-        } catch (e: java.lang.Exception) {
+        } catch (e: Exception) {
             e.printStackTrace()
         }
     }
@@ -210,26 +163,13 @@ fun sendMsg(webSocket: WebSocket, action: Action, msgParam: MsgParam?): Boolean 
     return webSocket.send(encode(blazeMsg))
 }
 
-fun sendTextMsg(
-    webSocket: WebSocket,
-    conversationId: String,
-    recipientId: String,
-    text: String
-): Boolean {
-    val msgParam =
-        MsgParam(UUID.randomUUID().toString(), Category.PLAIN_TEXT.toString(), conversationId, recipientId, text)
+fun sendTextMsg(webSocket: WebSocket, conversationId: String, recipientId: String, text: String): Boolean {
+    val msgParam = MsgParam(UUID.randomUUID().toString(), Category.PLAIN_TEXT.toString(), conversationId, recipientId, text)
     return sendMsg(webSocket, Action.CREATE_MESSAGE, msgParam)
 }
 
-fun sendCardMsg(
-    webSocket: WebSocket,
-    conversationId: String,
-    recipientId: String,
-    cards: Cards
-): Boolean {
-    val msgParam = MsgParam(
-        UUID.randomUUID().toString(), Category.APP_CARD.toString(), conversationId, recipientId, Gson().toJson(cards)
-    )
+fun sendCardMsg(webSocket: WebSocket, conversationId: String, recipientId: String, cards: Cards): Boolean {
+    val msgParam = MsgParam(UUID.randomUUID().toString(), Category.APP_CARD.toString(), conversationId, recipientId, Gson().toJson(cards))
     return sendMsg(webSocket, Action.CREATE_MESSAGE, msgParam)
 }
 
@@ -237,10 +177,7 @@ fun sendListPendingMsg(webSocket: WebSocket): Boolean {
     return sendMsg(webSocket, Action.LIST_PENDING_MESSAGES, null)
 }
 
-fun sendAckMsg(
-    webSocket: WebSocket,
-    messageId: String
-): Boolean {
+fun sendAckMsg(webSocket: WebSocket, messageId: String): Boolean {
     val msgParam = MsgParam(messageId)
     msgParam.status = Status.READ.toString()
     return sendMsg(webSocket, Action.ACKNOWLEDGE_MESSAGE_RECEIPT, msgParam)
