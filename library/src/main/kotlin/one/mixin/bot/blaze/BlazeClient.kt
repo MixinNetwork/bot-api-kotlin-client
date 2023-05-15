fc86f2721f998bfcd45de306b362d8763b1a5169
SeniorZhai <developer.zhaitao@gmail.com>
2023-05-15 14:13:56 +0800

    Code format

--- library/src/main/kotlin/one/mixin/bot/blaze/BlazeClient.kt
+++ library/src/main/kotlin/one/mixin/bot/blaze/BlazeClient.kt
@@ -162,7 +162,7 @@ class BlazeClient private constructor(
 }
 
 fun sendMsg(webSocket: WebSocket, action: Action, msgParam: MsgParam?): Boolean {
-    if (action == Action.CREATE_MESSAGE && msgParam?.conversionId.isNullOrEmpty()){
+    if (action == Action.CREATE_MESSAGE && msgParam?.conversionId.isNullOrEmpty()) {
         return false
     }
     if (msgParam?.data != null) {
