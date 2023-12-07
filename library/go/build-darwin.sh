d2ab0dc4e515367d3b8246be60739903dae8aba2
touge <touge1027@gmail.com>
2023-12-07 17:13:06 +0800

    Support kernel tx via gojni

---
+++ library/go/build-darwin.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# should install this version
+#go install github.com/tougee/jvm/cmd/gomobile
+#go install github.com/tougee/jvm/cmd/gobind
+
+# should export JAVA_HOME if not set
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/openjdk-11.0.2.jdk/Contents/Home
+
+GOMOBILE=gomobile
+
+$GOMOBILE bind -target=darwin/amd64 -x -v -o build -tags=openssl mixin/kernel
