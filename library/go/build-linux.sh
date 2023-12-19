ac5b7af55a87f38d08a93bb4d6047eaf82eec146
touge <touge1027@gmail.com>
2023-12-19 18:17:00 +0800

    Build for linux

--- library/go/build-linux.sh
+++ library/go/build-linux.sh
@@ -4,9 +4,10 @@
 #go install github.com/tougee/jvm/cmd/gomobile
 #go install github.com/tougee/jvm/cmd/gobind
 
-# should export JAVA_HOME if not set
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/openjdk-11.0.2.jdk/Contents/Home
+# build on MacOS
+# brew install FiloSottile/musl-cross/musl-cross
+# export CGO_ENABLED=1 GOOS=linux GOARCH=amd64 CC=x86_64-linux-musl-gcc CXX=x86_64-linux-musl-g++ 
+# export JAVA_HOME=/Library/Java/JavaVirtualMachines/openjdk-11.0.2.jdk/Contents/Home
 
 GOMOBILE=gomobile
-
 $GOMOBILE bind -target=linux/amd64 -x -v -o build -tags=openssl mixin/kernel
