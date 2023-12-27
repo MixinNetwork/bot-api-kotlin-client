645121cb6ad2463e633a8c0ac5f6899acbd1f9dd
touge <touge1027@gmail.com>
2023-12-27 17:51:02 +0800

    Fix linux so

--- library/go/build-linux.sh
+++ library/go/build-linux.sh
@@ -9,4 +9,7 @@
 # export CGO_ENABLED=1 GOOS=linux GOARCH=amd64 CC=x86_64-linux-musl-gcc CXX=x86_64-linux-musl-g++ 
 # export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
 
+# build on Linux
+# export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/
+
 gomobile bind -target=linux/amd64 -x -v -o build -tags=openssl mixin/kernel
